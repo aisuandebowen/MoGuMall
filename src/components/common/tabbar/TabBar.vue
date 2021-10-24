d9d8a2be95531c7d34488546a2bc7d2258c596fe
aisuandebowen <1836140285@qq.com>
2021-10-24 17:08:59 +0800

    the first commit

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="tab-bar">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'TabBar',
+        data() {
+            return {
+            }
+        },
+        created() {
+        },
+        computed: {
+        },
+        methods: {
+        },
+    }
+</script>
+<style scoped>
+    #tab-bar {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        right: 0;
+
+        display: flex;
+        background-color: #f6f6f6;
+
+        box-shadow: 0px -1px 1px rgba(0, 0, 0, .1);
+
+    }
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>我的Profile</h2>
+    </div>
+</template>
+<script>
+export default {
+   name: 'Profile',
+   data() {
+      return {
+      }
+   },
+   created(){
+   },
+   computed:{
+   },
+   methods:{
+   },
+}
+</script>
+<style scoped>
+
+</style>
